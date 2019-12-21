ebdf6893d80da3a62dd7124505991bd45d19a9bd
LoranBaker <loran7081@gmail.com>
2019-12-21 22:12:59 +0100

    JavaCoreProgrammingExercises

--- JavaCoreProgrammingLoranBaker/src/nine/NineExecutor.java
+++ JavaCoreProgrammingLoranBaker/src/nine/NineExecutor.java
@@ -5,12 +5,33 @@
  */
 package nine;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  *
  * @author PC
  */
 public class NineExecutor {
     public static void main(String[] args) {
-        
+        List <String> listOfNames = new ArrayList<>();
+        listOfNames.add("A");
+        listOfNames.add("B");
+        listOfNames.add("C");
+        listOfNames.add("D");
+        listOfNames.add("E");
+        listOfNames.add("F");
+        listOfNames.add("G");
+        System.out.println(listOfNames.get(2));
+        printCollection(listOfNames);
+    }
+    static void printCollection(Collection<String> names){
+        Iterator<String> namesIterator = names.iterator();
+        while(namesIterator.hasNext()){
+            String next = namesIterator.next();
+            System.out.println(next);
+        }
     }
 }
